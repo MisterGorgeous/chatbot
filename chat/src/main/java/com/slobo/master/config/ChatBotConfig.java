d5222413070f3f5b51c2fd1f7abfde89188edab3
MisterGorgeous <slabadniak2401@gmail.com>
2019-06-02 22:15:24 +0300

    add topiv preprocessor

--- chat/src/main/java/com/slobo/master/config/ChatBotConfig.java
+++ chat/src/main/java/com/slobo/master/config/ChatBotConfig.java
@@ -1,5 +1,6 @@
 package com.slobo.master.config;
 
+import cc.mallet.topics.TopicInferencer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,23 +9,25 @@ import com.google.code.chatterbotapi.ChatterBotFactory;
 import com.google.code.chatterbotapi.ChatterBotType;
 
 @Configuration
-public class ChatBotConfig
-{
+public class ChatBotConfig {
 
     @Bean
-    public ChatterBot cleverChatterBot() throws Exception
-    {
+    public ChatterBot cleverChatterBot() throws Exception {
         ChatterBotFactory factory = new ChatterBotFactory();
 
         return factory.create(ChatterBotType.CLEVERBOT);
     }
 
     @Bean
-    public ChatterBot pandorabotsChatterBot() throws Exception
-    {
+    public ChatterBot pandorabotsChatterBot() throws Exception {
         ChatterBotFactory factory = new ChatterBotFactory();
 
         return factory.create(ChatterBotType.PANDORABOTS, "b0dafd24ee35a477");
+    }
+
+    @Bean
+    public TopicInferencer topicInferencer() {
+        return new TopicInferencer(null, null, null, null, 0, 0);
     }
 
 }
